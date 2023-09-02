c2630ce1ceac8ff923110ac1eabab038b8fce963
ionelc <ionel.chis00@gmail.com>
2023-09-02 14:36:52 +0300

    Fix bom

--- bom/build.gradle.kts
+++ bom/build.gradle.kts
@@ -1,44 +1,14 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
+    id("java-platform")
     `maven-publish`
 }
 apply(plugin = "maven-publish")
 
-android {
-    namespace = "com.ionelchis.bom"
-    compileSdk = 33
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
 afterEvaluate {
     publishing {
         publications {
-            create<MavenPublication>("release") {
-                from(components["release"])
+            create<MavenPublication>("mavenJava") {
+                from(components["javaPlatform"])
             }
         }
     }
